013d44df3be0c56777821d9294cc2925c3d73256
chenpeng <a1454419599@gemail.com>
2018-08-20 16:58:10 +0800

    api

---
+++ doc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "title": "Custom apiDoc browser title",
+  "url": "https://39.108.114.59",
+  "name": "ts-node-wedding",
+  "version": "1.2.2",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-08-20T08:25:57.306Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
